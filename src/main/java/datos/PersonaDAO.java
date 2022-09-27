eca669463dce98fb848c4eb8d6ef03813f50b410
Faber0399 <faroca0399@gmail.com>
2022-09-27 13:52:41 -0500

    Method Insert

--- src/main/java/datos/PersonaDAO.java
+++ src/main/java/datos/PersonaDAO.java
@@ -6,9 +6,10 @@ import java.util.*;
 import domain.Persona;
 
 public class PersonaDAO {
-    private static final String SQL_SELECT= "SELECT idPersona, nombre, apellido, email, telefono FROM test.persona";
+    private static final String SQL_SELECT = "SELECT idPersona, nombre, apellido, email, telefono FROM test.persona";
+    private static final String SQL_INSERT = "INSERT INTO persona (nombre, apellido, email, telefono) VALUES (?,?,?,?)";
 
-    public List<Persona> seleccionar (){
+    public List<Persona> seleccionar() {
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
@@ -17,32 +18,58 @@ public class PersonaDAO {
         try {
             conn = Conexion.getConnection();
             stmt = conn.prepareStatement(SQL_SELECT);
-            rs=stmt.executeQuery();
-            while(rs.next()){
-                int idPersona=rs.getInt("idPersona");
-                String nombre =rs.getString("nombre");
-                String apellido =rs.getString("apellido");
-                String email =rs.getString("email");
-                String telefono =rs.getString("telefono");
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                int idPersona = rs.getInt("idPersona");
+                String nombre = rs.getString("nombre");
+                String apellido = rs.getString("apellido");
+                String email = rs.getString("email");
+                String telefono = rs.getString("telefono");
                 persona = new Persona(idPersona, nombre, apellido, email, telefono);
                 personas.add(persona);
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        }
-        finally{
+        } finally {
             try {
                 Conexion.close(rs);
                 Conexion.close(stmt);
                 Conexion.close(conn);
             } catch (SQLException e) {
-                
+
                 e.printStackTrace();
             }
-            
 
         }
         return personas;
+
+    }
+
+    public int insertar(Persona persona) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registro = 0;
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_INSERT);
+            stmt.setString(1, persona.getNombre());
+            stmt.setString(2, persona.getApeliido());
+            stmt.setString(3, persona.getEmail());
+            stmt.setString(4, persona.getTelefono());
+            registro = stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+
+            try {
+                Conexion.close(stmt);
+                Conexion.close(conn);
+            } catch (SQLException e) {
+
+                e.printStackTrace();
+            }
+        }
+        return registro;
 
     }
 }
--- src/main/java/test/TestManejoPersonas.java
+++ src/main/java/test/TestManejoPersonas.java
@@ -8,10 +8,13 @@ import domain.Persona;
 public class TestManejoPersonas {
     public static void main(String[] args) {
         PersonaDAO personaDao = new PersonaDAO();
+        // Se inserta un nuevo objeto de tipo Persona
+        Persona personaN = new Persona("Ligia","Causil","ligia@gmail.com","310655"); 
+        personaDao.insertar(personaN);
         List <Persona> personas=personaDao.seleccionar();
         for (Persona persona: personas){
             System.out.println(persona);
         }
-        
     }
+    
 }
